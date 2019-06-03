50516045de2b422d4dc2555f0a700f181350f313
kirbygit <heybisayangdakoo@gmail.com>
2019-06-02 19:54:08 -0700

    connect app to db, hardcoding credentials

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
 // connect to database
-// coming soon...
+$conn = mysqli_connect("localhost", "root", "root", "csfinal");
+
+	if (!$conn) {
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
+
+// defined global constants
 define ('ROOT_PATH', realpath(dirname(__FILE__)));
 define('BASE_URL', 'http://localhost/cs-final/');
 ?>
