286f842bcf8b5b3a644cf6a44756afe9a76ddd6a
kirbygit <heybisayangdakoo@gmail.com>
2019-06-02 22:20:26 -0700

    added includes for registration_login

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 <?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
 
+<?php require_once( ROOT_PATH . '/includes/registration_login.php') ?>
+
 <!-- Retrieve all posts from database  -->
 <?php $posts = getPublishedPosts(); ?>
 
