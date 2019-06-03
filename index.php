c8194a8e7efb75f5e64334802d2846f1ced824be
kirbygit <heybisayangdakoo@gmail.com>
2019-06-02 20:32:39 -0700

    modified and added another function, display topic in the content section

--- index.php
+++ index.php
@@ -27,17 +27,26 @@
 			<?php foreach ($posts as $post): ?>
 				<div class="post" style="margin-left: 0px;">
 					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
-					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
-						<div class="post_info">
-							<h3><?php echo $post['title'] ?></h3>
-								<div class="info">
-									<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
-									<span class="read_more">Read more...</span>
-								</div>
-						</div>
-					</a>
+        	<!-- Added this if statement... -->
+					<?php if (isset($post['topic']['name'])): ?>
+						<a
+							href="<?php echo BASE_URL . 'filtered_posts.php?topic=' . $post['topic']['id'] ?>"
+							class="btn category">
+						<?php echo $post['topic']['name'] ?>
+						</a>
+					<?php endif ?>
+
+		<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+			<div class="post_info">
+				<h3><?php echo $post['title'] ?></h3>
+				<div class="info">
+					<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+					<span class="read_more">Read more...</span>
 				</div>
-			<?php endforeach ?>
+			</div>
+		</a>
+	</div>
+<?php endforeach ?>
 
 		</div>
 		<!-- // end of Page content -->
