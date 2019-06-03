ca219a9a86a36a9fe2c608f978d949c884850323
kirbygit <heybisayangdakoo@gmail.com>
2019-06-03 11:57:16 -0700

    completed about page layout and content

--- includes/head_section.php
+++ includes/head_section.php
@@ -4,6 +4,7 @@
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
 	<link rel="stylesheet" href="static/css/public_styling.css">
+	<link rel="stylesheet" href="static/css/about.css">
 	<meta charset="UTF-8">
 	<title>Kirby's Blog</title>
 </head>
