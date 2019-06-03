eb2497e2530348b3b6a601636d2e7312ac527ce1
kirbygit <heybisayangdakoo@gmail.com>
2019-06-02 17:38:55 -0700

    initialize final project

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+	<link rel="stylesheet" href="static/css/public_styling.css">
+	<meta charset="UTF-8">
+	<title>LifeBlog | Home </title>
+</head>
+
+<body>
+	<!-- container -->
+	<div class="container">
+		<!-- navbar -->
+		<div class="navbar">
+			<div class="logo_div">
+				<a href="#"><h1>My Daily Life</h1></a>
+			</div>
+			<ul>
+			  <li><a class="active" href="index.php">Home</a></li>
+			  <li><a href="#news">News</a></li>
+			  <li><a href="#contact">Contact</a></li>
+			  <li><a href="#about">About</a></li>
+			</ul>
+		</div>
+		<!-- // end of navbar -->
+
+		<!-- Page content -->
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</div>
+		<!-- // end of Page content -->
+
+		<!-- footer -->
+		<div class="footer">
+			<p>Kirby James Fernandez &copy; <?php echo date('Y'); ?></p>
+		</div>
+		<!-- // end of footer -->
+
+	</div>
+	<!-- // end of container -->
+</body>
+
+</html>
