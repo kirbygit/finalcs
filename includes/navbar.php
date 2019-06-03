a1634b0bf55be306c8db9355fd156ae7b06d1916
kirbygit <heybisayangdakoo@gmail.com>
2019-06-03 12:13:24 -0700

    removed news part

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,7 +5,6 @@
 	</div>
 	<ul>
 		<li><a class="active" href="index.php">Home</a></li>
-		<li><a href="news.php">News</a></li>
 		<li><a href="contact.php">Contact</a></li>
 		<li><a href="about.php">About</a></li>
 	</ul>
