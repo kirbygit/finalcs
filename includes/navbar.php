bd40cb4d28eff33d8fc87a93b9b83bdecdf57b6e
kirbygit <heybisayangdakoo@gmail.com>
2019-06-02 22:08:49 -0700

    modified logo href

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <!-- navbar -->
 <div class="navbar">
   <div class="logo_div">
-	  <a href="#"><h1>Kirby</h1></a>
+	  <a href="index.php"><h1>Kirby</h1></a>
 	</div>
 	<ul>
 		<li><a class="active" href="index.php">Home</a></li>
